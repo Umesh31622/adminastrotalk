d8e202fe19b9dfb383556e9f7aee3d4861f150eb
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-11-13 15:19:07 +0530

    Create ourClientsRoutes.js

---
+++ routes/ourClientsRoutes.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const router = express.Router();
+
+const cloudUpload = require("../middleware/cloudUpload");
+
+// 🔥 Create Cloudinary uploader for Clients folder
+const upload = cloudUpload("clients");
+
+const {
+  createClient,
+  getClients,
+  updateClient,
+  deleteClient,
+} = require("../controllers/ourClientsController");
+
+router.post("/", upload.single("image"), createClient);
+router.get("/", getClients);
+router.put("/:id", upload.single("image"), updateClient);
+router.delete("/:id", deleteClient);
+
+module.exports = router;

63ee10061c5ec0b6707903171528e27804b6daff
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-11-18 10:49:19 +0530

    Update Remedy.js

--- models/Remedy.js
+++ models/Remedy.js
@@ -1,27 +1,3 @@
-// // const mongoose = require("mongoose");
-
-// // const remedySchema = new mongoose.Schema(
-// //   {
-// //     clientName: { type: String, required: true },
-// //     email: { type: String, required: true },
-// //     remedyType: {
-// //       type: String,
-// //       enum: ["Gemstone", "Yantra", "Mantra", "Homa", "Other"],
-// //       default: "Gemstone",
-// //     },
-// //     description: { type: String },
-// //     fileUrl: { type: String },
-// //     status: {
-// //       type: String,
-// //       enum: ["Pending", "Completed"],
-// //       default: "Pending",
-// //     },
-// //   },
-// //   { timestamps: true }
-// // );
-
-// // module.exports = mongoose.model("Remedy", remedySchema);
-
 // const mongoose = require("mongoose");
 
 // const remedySchema = new mongoose.Schema(
@@ -30,7 +6,7 @@
 //     email: { type: String, required: true },
 //     remedyType: {
 //       type: String,
-//       enum: ["Gemstone", "Yantra", "Mantra", "Homa", "Other"],
+//       enum: ["Gemstone", "Yantra", "Mantra","Rahu Yantra", "Homa", "Other"],
 //       default: "Gemstone",
 //     },
 //     description: { type: String },
@@ -49,25 +25,21 @@
 
 const mongoose = require("mongoose");
 
-const remedySchema = new mongoose.Schema(
+const orderSchema = new mongoose.Schema(
   {
-    clientName: { type: String, required: true },
+    customerName: { type: String, required: true },
     email: { type: String, required: true },
-    remedyType: {
-      type: String,
-      enum: ["Gemstone", "Yantra", "Mantra","Rahu Yantra", "Homa", "Other"],
-      default: "Gemstone",
-    },
-    description: { type: String },
-    fileUrl: { type: String },
+    serviceType: { type: String, required: true },
+    price: { type: Number, required: true },
     status: {
       type: String,
-      enum: ["Pending", "Completed"],
+      enum: ["Pending", "In Progress", "Completed"],
       default: "Pending",
     },
+    fileUrl: { type: String },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Remedy", remedySchema);
+module.exports = mongoose.model("Order", orderSchema);
 

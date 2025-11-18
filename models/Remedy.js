f76fdb9590a7885b47d5c692a08b1f56bc2357b2
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-11-18 11:02:29 +0530

    Update Remedy.js

--- models/Remedy.js
+++ models/Remedy.js
@@ -22,24 +22,27 @@
 
 // module.exports = mongoose.model("Remedy", remedySchema);
 
-
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema(
+const remedySchema = new mongoose.Schema(
   {
-    customerName: { type: String, required: true },
+    clientName: { type: String, required: true },
     email: { type: String, required: true },
-    serviceType: { type: String, required: true },
-    price: { type: Number, required: true },
+    remedyType: {
+      type: String,
+      enum: ["Gemstone", "Yantra", "Mantra","Rahu Yantra", "Homa", "Other"],
+      default: "Gemstone",
+    },
+    description: { type: String },
+    fileUrl: { type: String },
     status: {
       type: String,
-      enum: ["Pending", "In Progress", "Completed"],
+      enum: ["Pending", "Completed"],
       default: "Pending",
     },
-    fileUrl: { type: String },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Order", orderSchema);
+module.exports = mongoose.model("Remedy", remedySchema);
 

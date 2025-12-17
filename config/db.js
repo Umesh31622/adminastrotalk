21f2aeaac40689f97fe5436a5b730640843b7b00
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-17 18:03:12 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,23 +1,3 @@
-
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-
-//     console.log("✅ MongoDB Connected");
-//   } catch (error) {
-//     console.error("❌ MongoDB Connection Failed:", error.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-
-
     
 const mongoose = require("mongoose");
 
@@ -36,5 +16,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-

bbf10493b3d7500684e8e9078ea0c4b3da7c1ce3
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:30:49 +0530

    Update discountRoutes.js

--- routes/discountRoutes.js
+++ routes/discountRoutes.js
@@ -1,27 +1,3 @@
-// // const express = require("express");
-// // const router = express.Router();
-// // const { getDiscountCodes, createDiscountCode, deleteDiscountCode } = require("../controllers/discountController");
-
-// // router.get("/", getDiscountCodes);
-// // router.post("/", createDiscountCode);
-// // router.delete("/:id", deleteDiscountCode);
-
-// // module.exports = router;
-
-// const express = require("express");
-// const router = express.Router();
-// const {
-//   getDiscountCodes,
-//   createDiscountCode,
-//   deleteDiscountCode,
-// } = require("../controllers/discountController");
-
-// router.get("/", getDiscountCodes);
-// router.post("/", createDiscountCode);
-// router.delete("/:id", deleteDiscountCode);
-
-// module.exports = router;
-
 
 const express = require("express");
 const router = express.Router();

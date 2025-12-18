eac0a2128e55b2dbb811ce842da5f129ec47ed2f
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:35:07 +0530

    Update knowMoreRoutes.js

--- routes/knowMoreRoutes.js
+++ routes/knowMoreRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const router = require("express").Router();
 const upload = require("../middlewares/uploadPdf");
 const controller = require("../controllers/knowMoreController");
@@ -9,23 +8,3 @@ router.put("/:id", upload.single("pdf"), controller.updateKnowMore);
 router.delete("/:id", controller.deleteKnowMore);
 
 module.exports = router;
-=======
-
-const express = require("express");
-const router = express.Router();
-
-const cloudUpload = require("../middleware/cloudUpload");
-const {
-  uploadKnowMorePDF,
-  getAllPDFs,
-  updatePDF,
-  deletePDF,
-} = require("../controllers/knowMoreController");
-
-router.post("/upload", cloudUpload("know_more").single("pdf"), uploadKnowMorePDF);
-router.get("/", getAllPDFs);
-router.put("/:id", cloudUpload("know_more").single("pdf"), updatePDF);
-router.delete("/:id", deletePDF);
-
-module.exports = router;
->>>>>>> 287860747a9161e2609805405122ff2ca97fad0a

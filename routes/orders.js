1fc47e21f0375d453f01e82b8629d242106f979b
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:38:20 +0530

    Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -1,27 +1,4 @@
 
-
-// const express = require("express");
-// const router = express.Router();
-// const upload = require("../middleware/upload"); // multer instance
-// const {
-//   createOrder,
-//   getOrders,
-//   getOrderById,
-//   updateOrder,
-//   deleteOrder,
-// } = require("../controllers/orderController");
-
-// // Routes
-// router.get("/", getOrders);
-// router.get("/:id", getOrderById);
-
-// // Use `file` here to match React FormData field
-// router.post("/", upload.single("file"), createOrder);
-// router.put("/:id", upload.single("file"), updateOrder);
-// router.delete("/:id", deleteOrder);
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 
@@ -44,3 +21,4 @@ router.put("/:id", upload.single("file"), updateOrder);
 router.delete("/:id", deleteOrder);
 
 module.exports = router;
+

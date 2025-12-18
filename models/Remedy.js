041876078702e9f2f787164660a026580e418b12
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:36:28 +0530

    Update Remedy.js

--- models/Remedy.js
+++ models/Remedy.js
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 287860747a9161e2609805405122ff2ca97fad0a
 const mongoose = require("mongoose");
 
 const remedySchema = new mongoose.Schema(
   {
-<<<<<<< HEAD
     // ================= CLIENT DETAILS =================
     clientName: {
       type: String,
@@ -52,34 +48,15 @@ const remedySchema = new mongoose.Schema(
     },
 
     // ================= STATUS =================
-=======
-    clientName: { type: String, required: true },
-    email: { type: String, required: true },
-    remedyType: {
-      type: String,
-      enum: ["Gemstone", "Yantra", "Mantra","Rahu Yantra", "Homa", "Other"],
-      default: "Gemstone",
-    },
-    description: { type: String },
-    fileUrl: { type: String },
->>>>>>> 287860747a9161e2609805405122ff2ca97fad0a
     status: {
       type: String,
       enum: ["Pending", "Completed"],
       default: "Pending",
     },
   },
-<<<<<<< HEAD
   {
     timestamps: true,
   }
 );
 
 module.exports = mongoose.model("Remedy", remedySchema);
-=======
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("Remedy", remedySchema);
-
->>>>>>> 287860747a9161e2609805405122ff2ca97fad0a

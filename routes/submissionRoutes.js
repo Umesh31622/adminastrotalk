8dd2dde3bd69004c67c04be64353364d3ad64aef
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:43:58 +0530

    Update submissionRoutes.js

--- routes/submissionRoutes.js
+++ routes/submissionRoutes.js
@@ -1,11 +1,3 @@
-// const express = require("express");
-// const { createSubmission, getAllSubmissions } = require("../controllers/submissionController");
-// const router = express.Router();
-
-// router.post("/", createSubmission);
-// router.get("/", getAllSubmissions);
-
-// module.exports = router;
 
 const express = require("express");
 const router = express.Router();

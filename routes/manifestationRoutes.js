cc51e2d0db400a78dc8159bf62ba32e9b9e1fbf9
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:36:34 +0530

    Update manifestationRoutes.js

--- routes/manifestationRoutes.js
+++ routes/manifestationRoutes.js
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
-// const router = require("express").Router();
-// const {
-//   addService,
-//   getAll,
-//   updateService,
-//   deleteService,
-// } = require("../controllers/manifestationController");
 
-// router.post("/add", addService);
-// router.get("/all", getAll);
-// router.put("/update/:id", updateService);
-// router.delete("/delete/:id", deleteService);
-
-// module.exports = router;
 const express = require("express");
 const router = express.Router();
 
-=======
-const router = require("express").Router();
->>>>>>> 287860747a9161e2609805405122ff2ca97fad0a
 const {
   addService,
   getAll,
@@ -26,7 +9,6 @@ const {
   deleteService,
 } = require("../controllers/manifestationController");
 
-<<<<<<< HEAD
 // ➕ Add new manifestation / well-being service
 router.post("/add", addService);
 
@@ -37,11 +19,6 @@ router.get("/all", getAll);
 router.put("/update/:id", updateService);
 
 // 🗑️ Delete service by ID
-=======
-router.post("/add", addService);
-router.get("/all", getAll);
-router.put("/update/:id", updateService);
->>>>>>> 287860747a9161e2609805405122ff2ca97fad0a
 router.delete("/delete/:id", deleteService);
 
 module.exports = router;

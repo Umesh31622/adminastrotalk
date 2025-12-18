c62b4a07c89f65a0dda5b055582641999e6141fa
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:30:59 +0530

    Update KnowMore.js

--- models/KnowMore.js
+++ models/KnowMore.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 const knowMoreSchema = new mongoose.Schema(
@@ -21,18 +20,3 @@ const knowMoreSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("KnowMore", knowMoreSchema);
-=======
-
-const mongoose = require("mongoose");
-
-const KnowMoreSchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true },
-    pdfUrl: { type: String, required: true },
-    cloudinaryId: { type: String, required: true }, // public_id
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("KnowMore", KnowMoreSchema);
->>>>>>> 287860747a9161e2609805405122ff2ca97fad0a

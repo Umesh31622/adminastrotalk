6d979f2a265a9e9d57f25a5844a53c8696b9ced7
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:32:21 +0530

    Update Manglik.js

--- models/Manglik.js
+++ models/Manglik.js
@@ -1,17 +1,5 @@
-// const mongoose = require("mongoose");
 
-// const manglikSchema = new mongoose.Schema(
-//   {
-//     name: { type: String, required: true },
-//     dateOfBirth: { type: String, required: true },
-//     timeOfBirth: { type: String, required: true },
-//     placeOfBirth: { type: String, required: true },
-//     isManglik: Boolean,
-//   },
-//   { timestamps: true }
-// );
 
-// module.exports = mongoose.model("Manglik", manglikSchema);
 const mongoose = require("mongoose");
 
 const manglikSchema = new mongoose.Schema(

3afd33d24c72ff1cd5322681742997c69a053430
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:17:43 +0530

    Update astrologerRoutes.js

--- routes/astrologerRoutes.js
+++ routes/astrologerRoutes.js
@@ -1,18 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const {
-//   getAstrologers,
-//   createAstrologer,
-//   updateAstrologer,
-//   deleteAstrologer,
-// } = require("../controllers/astrologerController");
-
-// router.get("/", getAstrologers);
-// router.post("/", createAstrologer);
-// router.put("/:id", updateAstrologer);
-// router.delete("/:id", deleteAstrologer);
-
-// module.exports = router;
 
 const express = require("express");
 const router = express.Router();

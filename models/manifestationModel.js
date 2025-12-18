2861f3c4db901b7a99b8f532c1725ec22ea709af
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:46:55 +0530

    Update manifestationModel.js

--- models/manifestationModel.js
+++ models/manifestationModel.js
@@ -1,29 +1,9 @@
-<<<<<<< HEAD
-// const mongoose = require("mongoose");
 
-// const manifestationSchema = new mongoose.Schema(
-//   {
-//     label: { type: String, required: true },
-//     description: String,
-//     fullDesc: String,
-//     deliverable: String,
-//     category: {
-//       type: String,
-//       enum: ["manifestation", "well-being"],
-//       default: "manifestation",
-//     },
-//   },
-//   { timestamps: true }
-// );
 
-// module.exports = mongoose.model("ManifestationService", manifestationSchema);
-=======
->>>>>>> 287860747a9161e2609805405122ff2ca97fad0a
 const mongoose = require("mongoose");
 
 const manifestationSchema = new mongoose.Schema(
   {
-<<<<<<< HEAD
     label: {
       type: String,
       required: true,
@@ -52,12 +32,6 @@ const manifestationSchema = new mongoose.Schema(
       min: 0,
     },
 
-=======
-    label: { type: String, required: true },
-    description: String,
-    fullDesc: String,
-    deliverable: String,
->>>>>>> 287860747a9161e2609805405122ff2ca97fad0a
     category: {
       type: String,
       enum: ["manifestation", "well-being"],

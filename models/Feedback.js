69005a454d1449039294c1e0f422d28c8245fd85
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:29:34 +0530

    Update Feedback.js

--- models/Feedback.js
+++ models/Feedback.js
@@ -1,22 +1,4 @@
 
-// // const mongoose = require("mongoose");
-
-// // const feedbackSchema = new mongoose.Schema(
-// //   {
-// //     name: { type: String, required: true },
-// //     email: { type: String, required: true },
-// //     message: { type: String, required: true },
-// //     service: { type: String },
-// //     rating: { type: Number, required: true },
-// //     status: { type: String, default: "pending" }, // pending/approved/rejected
-// //     isPublished: { type: Boolean, default: false },
-// //     tags: [{ type: String }],
-// //   },
-// //   { timestamps: true }
-// // );
-
-// module.exports = mongoose.model("Feedback", feedbackSchema);
-
 const mongoose = require("mongoose");
 
 const feedbackSchema = new mongoose.Schema(

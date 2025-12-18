d75e92c5e780c18f4e2c0cf8e823f40c04fd4ed5
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:07:30 +0530

    Update walletController.js

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -1,41 +1,4 @@
-// const Wallet = require("../models/Wallet");
 
-// exports.getWallet = async (req, res) => {
-//   try {
-//     let wallet = await Wallet.findOne({ userId: req.params.userId });
-
-//     if (!wallet) {
-//       wallet = await Wallet.create({
-//         userId: req.params.userId,
-//         balance: 0,
-//         transactions: [],
-//       });
-//     }
-
-//     res.json({ success: true, wallet });
-//   } catch (err) {
-//     res.status(500).json({ success: false, error: err.message });
-//   }
-// };
-
-// exports.addMoneyToWallet = async (userId, amount) => {
-//   let wallet = await Wallet.findOne({ userId });
-
-//   if (!wallet) {
-//     wallet = await Wallet.create({
-//       userId,
-//       balance: amount,
-//       transactions: [{ amount, type: "credit" }],
-//     });
-//     return wallet;
-//   }
-
-//   wallet.balance += amount;
-//   wallet.transactions.push({ amount, type: "credit" });
-//   await wallet.save();
-
-//   return wallet;
-// };
 const Wallet = require("../models/Wallet");
 
 /* ===========================================================

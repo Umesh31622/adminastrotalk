ef1a667f116135815e77bf1b2a5b3f33af0f9462
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:33:18 +0530

    Update feedbackRoutes.js

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -1,25 +1,4 @@
 
-
-// const express = require("express");
-// const router = express.Router();
-// const { auth, admin } = require("../middleware/auth");
-// const {
-//   createFeedback,
-//   getAllFeedbacks,
-//   deleteFeedback,
-//   updateFeedback,
-// } = require("../controllers/feedbackController");
-
-// // Public
-// router.post("/", createFeedback);
-// router.get("/public", getAllFeedbacks); // public read
-
-// // Admin protected
-// router.get("/", auth, admin, getAllFeedbacks);
-// router.delete("/:id", auth, admin, deleteFeedback);
-// router.put("/:id", auth, admin, updateFeedback);
-
-// module.exports = router;
 const express = require("express");
 const router = express.Router();
 const {
@@ -35,3 +14,4 @@ router.put("/:id", updateFeedback);
 router.delete("/:id", deleteFeedback);
 
 module.exports = router;
+

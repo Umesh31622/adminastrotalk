ab175e0ea376eb6079ad24207d7f328b03383616
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:41:51 +0530

    Update remedyJourney.routes.js

--- routes/remedyJourney.routes.js
+++ routes/remedyJourney.routes.js
@@ -1,21 +1,4 @@
-// const express = require("express");
-// const router = express.Router();
 
-// const {
-//   getAll,
-//   create,
-//   updateStatus,
-//   remove,
-//   getUserRemedies,
-// } = require("../controllers/remedyJourneyController");
-
-// /* MATCHES FRONTEND EXACTLY */
-// router.get("/", getAll);
-// router.post("/", create);
-// router.put("/:id/status", updateStatus);
-// router.delete("/:id", remove);
-// router.get("/user/:userId", getUserRemedies); 
-// module.exports = router;
 const express = require("express");
 const router = express.Router();
 

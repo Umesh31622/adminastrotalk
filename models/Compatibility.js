e8e192d939cfa1f7b7fbbdcf2d4b0a4567082be5
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:24:17 +0530

    Update Compatibility.js

--- models/Compatibility.js
+++ models/Compatibility.js
@@ -1,19 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const CompatibilitySchema = new mongoose.Schema({
-//   boyName: { type: String, required: true },
-//   boyDob: { type: String, required: true },
-//   boyTob: { type: String, required: true },
-//   boyPlace: { type: String, required: true },       // ✅ Added
-//   girlName: { type: String, required: true },
-//   girlDob: { type: String, required: true },
-//   girlTob: { type: String, required: true },
-//   girlPlace: { type: String, required: true },     // ✅ Added
-//   score: Number,
-//   message: String,
-// }, { timestamps: true });
-
-// module.exports = mongoose.model("Compatibility", CompatibilitySchema);
 
 const mongoose = require("mongoose");
 

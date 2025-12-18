a58f7a58b46dce58dec2944a9d1540ac811fa6a2
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 10:58:21 +0530

    Update submissionController.js

--- controllers/submissionController.js
+++ controllers/submissionController.js
@@ -1,30 +1,4 @@
-// const Submission = require("../models/Submission");
 
-// // Create submission
-// exports.createSubmission = async (req, res) => {
-//   try {
-//     const { formId, userEmail, data } = req.body;
-//     if (!formId || !data)
-//       return res.status(400).json({ success: false, error: "Form ID and data are required" });
-
-//     const submission = await Submission.create({ formId, userEmail, data });
-//     res.status(201).json({ success: true, submission });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ success: false, error: err.message });
-//   }
-// };
-
-// // Get all submissions
-// exports.getAllSubmissions = async (req, res) => {
-//   try {
-//     const submissions = await Submission.find().populate("formId");
-//     res.json({ success: true, submissions });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ success: false, error: err.message });
-//   }
-// };
 
 const Submission = require("../models/Submission");
 

4d774dad72ebb895b02738f270181c00248124b9
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:27:50 +0530

    Update consultationRoutes.js

--- routes/consultationRoutes.js
+++ routes/consultationRoutes.js
@@ -1,37 +1,4 @@
 
-
-// const express = require("express");
-// const router = express.Router();
-// const {
-//   getConsultations,
-//   createConsultation,
-//   updateConsultation,
-//   deleteConsultation,
-// } = require("../controllers/consultationController");
-
-// router.get("/", getConsultations);
-// router.post("/", createConsultation); // Auto link + email
-// router.put("/:id", updateConsultation);
-// router.delete("/:id", deleteConsultation);
-
-// module.exports = router;
-
-<<<<<<< HEAD
-// const express = require("express");
-// const router = express.Router();
-// const {
-//   getConsultations,
-//   createConsultation,
-//   updateConsultation,
-//   deleteConsultation,
-// } = require("../controllers/consultationController");
-
-// router.get("/", getConsultations);
-// router.post("/", createConsultation); // Auto real Google Meet link + email
-// router.put("/:id", updateConsultation);
-// router.delete("/:id", deleteConsultation);
-
-// module.exports = router;
 const express = require("express");
 const router = express.Router();
 
@@ -39,29 +6,18 @@ const {
   createConsultation,
   getConsultations,
   getUserConsultations,
-=======
-const express = require("express");
-const router = express.Router();
-const {
-  getConsultations,
-  createConsultation,
->>>>>>> 287860747a9161e2609805405122ff2ca97fad0a
   updateConsultation,
   deleteConsultation,
 } = require("../controllers/consultationController");
 
-<<<<<<< HEAD
 // USER
 router.get("/user/:email", getUserConsultations);
 router.post("/", createConsultation);
 
 // ADMIN
 router.get("/", getConsultations);
-=======
-router.get("/", getConsultations);
-router.post("/", createConsultation); // Auto real Google Meet link + email
->>>>>>> 287860747a9161e2609805405122ff2ca97fad0a
 router.put("/:id", updateConsultation);
 router.delete("/:id", deleteConsultation);
 
 module.exports = router;
+

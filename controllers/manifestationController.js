d7aaf8bea44006a4aaec59864ba458a7055e67c0
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 10:51:11 +0530

    Update manifestationController.js

--- controllers/manifestationController.js
+++ controllers/manifestationController.js
@@ -1,51 +1,4 @@
-<<<<<<< HEAD
-// const ManifestationService = require("../models/manifestationModel");
 
-// // ADD NEW
-// exports.addService = async (req, res) => {
-//   try {
-//     const service = await ManifestationService.create(req.body);
-//     res.json({ status: true, message: "Service added", service });
-//   } catch (e) {
-//     res.json({ status: false, message: e.message });
-//   }
-// };
-
-// // GET ALL - Manifestation + Wellbeing
-// exports.getAll = async (req, res) => {
-//   try {
-//     const manifestation = await ManifestationService.find({ category: "manifestation" });
-//     const wellbeing = await ManifestationService.find({ category: "well-being" });
-
-//     res.json({ status: true, manifestation, wellbeing });
-//   } catch (e) {
-//     res.json({ status: false, message: e.message });
-//   }
-// };
-
-// // UPDATE
-// exports.updateService = async (req, res) => {
-//   try {
-//     const updated = await ManifestationService.findByIdAndUpdate(
-//       req.params.id,
-//       req.body,
-//       { new: true }
-//     );
-//     res.json({ status: true, message: "Updated", service: updated });
-//   } catch (e) {
-//     res.json({ status: false, message: e.message });
-//   }
-// };
-
-// // DELETE
-// exports.deleteService = async (req, res) => {
-//   try {
-//     await ManifestationService.findByIdAndDelete(req.params.id);
-//     res.json({ status: true, message: "Deleted" });
-//   } catch (e) {
-//     res.json({ status: false, message: e.message });
-//   }
-// };
 const ManifestationService = require("../models/manifestationModel");
 
 /**
@@ -203,52 +156,5 @@ exports.deleteService = async (req, res) => {
       status: false,
       message: e.message,
     });
-=======
-const ManifestationService = require("../models/manifestationModel");
-
-// ADD NEW
-exports.addService = async (req, res) => {
-  try {
-    const service = await ManifestationService.create(req.body);
-    res.json({ status: true, message: "Service added", service });
-  } catch (e) {
-    res.json({ status: false, message: e.message });
-  }
-};
-
-// GET ALL - Manifestation + Wellbeing
-exports.getAll = async (req, res) => {
-  try {
-    const manifestation = await ManifestationService.find({ category: "manifestation" });
-    const wellbeing = await ManifestationService.find({ category: "well-being" });
-
-    res.json({ status: true, manifestation, wellbeing });
-  } catch (e) {
-    res.json({ status: false, message: e.message });
-  }
-};
-
-// UPDATE
-exports.updateService = async (req, res) => {
-  try {
-    const updated = await ManifestationService.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    );
-    res.json({ status: true, message: "Updated", service: updated });
-  } catch (e) {
-    res.json({ status: false, message: e.message });
-  }
-};
-
-// DELETE
-exports.deleteService = async (req, res) => {
-  try {
-    await ManifestationService.findByIdAndDelete(req.params.id);
-    res.json({ status: true, message: "Deleted" });
-  } catch (e) {
-    res.json({ status: false, message: e.message });
->>>>>>> 287860747a9161e2609805405122ff2ca97fad0a
   }
 };

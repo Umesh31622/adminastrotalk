694e60a44748fb2fe514252c1eb2298b56f2c359
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:45:59 +0530

    Update faqModel.js

--- models/faqModel.js
+++ models/faqModel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // const mongoose = require("mongoose");
 
 // const faqSchema = new mongoose.Schema(
@@ -52,18 +51,3 @@ const faqSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("FAQ", faqSchema);
-=======
-const mongoose = require("mongoose");
-
-const faqSchema = new mongoose.Schema(
-  {
-    question: { type: String, required: true },
-    answer: { type: String, required: true },
-    category: { type: String, default: "general" }, // optional
-    status: { type: String, default: "active" },
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("FAQ", faqSchema);
->>>>>>> 287860747a9161e2609805405122ff2ca97fad0a

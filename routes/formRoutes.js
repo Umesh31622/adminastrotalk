52be81f0c4a8ad10b538279d3894918b7fdb750c
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:33:47 +0530

    Update formRoutes.js

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -1,17 +1,4 @@
-// const express = require("express");
-// const {
-//   createForm,
-//   getAllForms,
-//   deleteForm,
-// } = require("../controllers/formController");
 
-// const router = express.Router();
-
-// router.post("/", createForm);
-// router.get("/", getAllForms);
-// router.delete("/:id", deleteForm);  // <-- DELETE route added
-
-// module.exports = router;
 const express = require("express");
 const router = express.Router();
 const { createForm, getAllForms, deleteForm } = require("../controllers/formController");

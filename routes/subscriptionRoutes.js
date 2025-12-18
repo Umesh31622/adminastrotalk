7632443379f8b34fae3aee0c7fd18a97ec56c072
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-18 11:44:28 +0530

    Update subscriptionRoutes.js

--- routes/subscriptionRoutes.js
+++ routes/subscriptionRoutes.js
@@ -1,17 +1,4 @@
 
-// const express = require("express");
-// const router = express.Router();
-// const { getPlans, createPlan, deletePlan, getReferrals, approveReferral } = require("../controllers/subscriptionController");
-
-// router.get("/plans", getPlans);
-// router.post("/plans", createPlan);
-// router.delete("/plans/:id", deletePlan);
-
-// router.get("/referrals", getReferrals);
-// router.put("/referrals/:id/approve", approveReferral);
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 const {
@@ -36,3 +23,4 @@ router.put("/referrals/:id/approve", approveReferral);
 router.delete("/referrals/:id", deleteReferral);
 
 module.exports = router;
+
